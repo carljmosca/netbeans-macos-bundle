a6af00185001b28b62d55fe1c3d6bd32b0125683
Carl J. Mosca <carljmosca@gmail.com>
2018-08-02 12:11:41 -0400

    sudo requirement

---
+++ README.md
@@ -0,0 +1,20 @@
+# NetBeans MacOS Bundle
+
+This project is a quick-and-dirty method to "install" NetBeans 9.x on a Mac.  Currently, to my knowledge, the NetBeans sources and binaries can be downloaded but I have not happened across an installation method for Macs.
+
+I have been running it from the command line during the release candidate period but after putting it into the /Applications folder manually, I though a script might be nice.
+
+To run it, use the install.sh script. 
+
+```
+./install.sh
+```
+
+There are two environment variables that may be set to change the defaults before running.
+For example:
+
+```
+export NETBEANS_VERSION=9.1
+export NETBEANS_URI=http://some.mirror.com/incubating-netbeans-java-9.0-bin.zip
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ NETBEANS_VERSION=${NETBEANS_VERSION:-9}
 NETBEANS_URI=${NETBEANS_URI:-http://apache.mirrors.pair.com/incubator/netbeans/incubating-netbeans-java/incubating-9.0/incubating-netbeans-java-9.0-bin.zip}
 
 curl $NETBEANS_URI > temp.zip
-mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS"
-mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources"
-sed "s/NETBEANS_VERSION/$NETBEANS_VERSION/g" Info.plist > "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Info.plist"
-unzip temp.zip -d "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
-mv "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans"
+sudo mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS"
+sudo mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources"
+sudo sed "s/NETBEANS_VERSION/$NETBEANS_VERSION/g" Info.plist > "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Info.plist"
+sudo unzip temp.zip -d "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
+sudo mv "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans"
 rm temp.zip
-ln -s "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/bin/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS/netbeans"
-cp "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/nb/netbeans.icns" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
+sudo ln -s "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/bin/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS/netbeans"
+sudo cp "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/nb/netbeans.icns" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
