3b1e528f9ef0f1b3d818103ac2e016952f83d448
Carl J. Mosca <carljmosca@gmail.com>
2018-08-02 11:04:42 -0400

    corrected renaming, order of operations

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@
 NETBEANS_VERSION=${NETBEANS_VERSION:-9}
 NETBEANS_URI=${NETBEANS_URI:-http://apache.mirrors.pair.com/incubator/netbeans/incubating-netbeans-java/incubating-9.0/incubating-netbeans-java-9.0-bin.zip}
 
+curl $NETBEANS_URI > temp.zip
 mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS"
 mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources"
 sed "s/NETBEANS_VERSION/$NETBEANS_VERSION/g" Info.plist > "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Info.plist"
-curl $NETBEANS_URI > temp.zip
-unzip temp.zip "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS/netbeans"
+unzip temp.zip -d "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
+mv "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans"
 rm temp.zip
-ln -s "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/bin/netbeans"
+ln -s "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/bin/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS/netbeans"
+cp "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/nb/netbeans.icns" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
