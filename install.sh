f23a61e44521bf14ef3fe299d17bc8a2f16ff89c
Carl J. Mosca <carljmosca@gmail.com>
2018-08-02 13:53:45 -0400

    addressed issue copying plist

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ NETBEANS_URI=${NETBEANS_URI:-http://apache.mirrors.pair.com/incubator/netbeans/i
 curl $NETBEANS_URI > temp.zip
 sudo mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS"
 sudo mkdir -p "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources"
-sudo sed "s/NETBEANS_VERSION/$NETBEANS_VERSION/g" Info.plist > "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Info.plist"
+sed "s/NETBEANS_VERSION/$NETBEANS_VERSION/g" Info.plist > Info.plist.2
+sudo mv Info.plist.2 "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Info.plist"
 sudo unzip temp.zip -d "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
 sudo mv "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans"
 rm temp.zip
