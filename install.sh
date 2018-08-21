7d2c1e97f3e5cbc83e2889f52061d6201b384e63
Carl J. Mosca <carljmosca@gmail.com>
2018-08-21 03:46:42 -0700

    Revert "reverse symbolic link change"
    This reverts commit 61f5e6ae84146fa4b03bbd31003c6116b492524e.

--- install.sh
+++ install.sh
@@ -11,6 +11,5 @@ sudo mv Info.plist.2 "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Cont
 sudo unzip temp.zip -d "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
 sudo mv "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans"
 rm temp.zip
-cd "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS"
-sudo ln -s ../Resources/NetBeans/bin/netbeans  netbeans
+sudo ln -s "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/bin/netbeans" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/MacOS/netbeans"
 sudo cp "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/NetBeans/nb/netbeans.icns" "/Applications/NetBeans/NetBeans $NETBEANS_VERSION.app/Contents/Resources/"
